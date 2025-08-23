cd9c708280ff291a56150bf8a50a6532d0a20c82
kamruj-akash <source.akash@gmail.com>
2025-08-23 17:44:46 +0600

    Added All btn layout

--- js/homePage.js
+++ js/homePage.js
@@ -9,12 +9,12 @@ document
     const addMoneyAmount = parseInt(
       document.getElementById("addMoney_amount").value
     );
-    const addMoneyPinNo = document.getElementById("addMoney_pin").value;
+    const accountPinNo = document.getElementById("account_pin").value;
     const availableBalance = parseInt(
       document.getElementById("available_balance").innerText
     );
     if (addMoneyAccountNo.length === 11) {
-      if (addMoneyPinNo.length === 4) {
+      if (accountPinNo.length === 4) {
         if (addMoneyAmount > 0) {
           let admoneyAvailableBalance = availableBalance + addMoneyAmount;
           document.getElementById("available_balance").innerText =
@@ -28,4 +28,69 @@ document
     } else {
       alert("Please provide 11 digit Account No");
     }
+  });
+
+// toggle btn
+document.getElementById("addMoney_btn").addEventListener("click", function () {
+  document.getElementById("latestPayment_parent_div").style.display = "none";
+  document.getElementById("addmoney_parent_div").style.display = "block";
+  document.getElementById("cashout_parent_div").style.display = "none";
+  document.getElementById("transferMoney_parent_div").style.display = "none";
+  document.getElementById("getBonus_parent_div").style.display = "none";
+  document.getElementById("payBill_parent_div").style.display = "none";
+  document.getElementById("transactions_parent_div").style.display = "none";
+});
+
+document.getElementById("cashOut_btn").addEventListener("click", function () {
+  document.getElementById("latestPayment_parent_div").style.display = "none";
+  document.getElementById("addmoney_parent_div").style.display = "none";
+  document.getElementById("cashout_parent_div").style.display = "block";
+  document.getElementById("transferMoney_parent_div").style.display = "none";
+  document.getElementById("getBonus_parent_div").style.display = "none";
+  document.getElementById("payBill_parent_div").style.display = "none";
+  document.getElementById("transactions_parent_div").style.display = "none";
+});
+
+document
+  .getElementById("transferMoney_btn")
+  .addEventListener("click", function () {
+    document.getElementById("latestPayment_parent_div").style.display = "none";
+    document.getElementById("addmoney_parent_div").style.display = "none";
+    document.getElementById("cashout_parent_div").style.display = "none";
+    document.getElementById("transferMoney_parent_div").style.display = "block";
+    document.getElementById("getBonus_parent_div").style.display = "none";
+    document.getElementById("payBill_parent_div").style.display = "none";
+    document.getElementById("transactions_parent_div").style.display = "none";
+  });
+
+document.getElementById("getBonus_btn").addEventListener("click", function () {
+  document.getElementById("latestPayment_parent_div").style.display = "none";
+  document.getElementById("addmoney_parent_div").style.display = "none";
+  document.getElementById("cashout_parent_div").style.display = "none";
+  document.getElementById("transferMoney_parent_div").style.display = "none";
+  document.getElementById("getBonus_parent_div").style.display = "block";
+  document.getElementById("payBill_parent_div").style.display = "none";
+  document.getElementById("transactions_parent_div").style.display = "none";
+});
+
+document.getElementById("payBill_btn").addEventListener("click", function () {
+  document.getElementById("latestPayment_parent_div").style.display = "none";
+  document.getElementById("addmoney_parent_div").style.display = "none";
+  document.getElementById("cashout_parent_div").style.display = "none";
+  document.getElementById("transferMoney_parent_div").style.display = "none";
+  document.getElementById("getBonus_parent_div").style.display = "none";
+  document.getElementById("payBill_parent_div").style.display = "block";
+  document.getElementById("transactions_parent_div").style.display = "none";
+});
+
+document
+  .getElementById("transactions_btn")
+  .addEventListener("click", function () {
+    document.getElementById("latestPayment_parent_div").style.display = "none";
+    document.getElementById("addmoney_parent_div").style.display = "none";
+    document.getElementById("cashout_parent_div").style.display = "none";
+    document.getElementById("transferMoney_parent_div").style.display = "none";
+    document.getElementById("getBonus_parent_div").style.display = "none";
+    document.getElementById("payBill_parent_div").style.display = "none";
+    document.getElementById("transactions_parent_div").style.display = "block";
   });
