7ce20bafb0aba85de296b3218226953606239142
kamruj-akash <source.akash@gmail.com>
2025-08-23 11:57:27 +0600

    added Loginpage, homepage, and js layout

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const login = document.getElementById("login_btn");
+const loginMobile = "1";
+const loginPin = "1";
+
+login.addEventListener("click", function (event) {
+  event.preventDefault();
+  const inputMobile = document.getElementById("input_mobileNo").value;
+  const inputPin = document.getElementById("input_PinNo").value;
+  if (inputMobile === loginMobile && inputPin === loginPin) {
+    window.location.href = "../homePage.html";
+  } else {
+    alert("Incorrect Mobile or Password");
+  }
+});
