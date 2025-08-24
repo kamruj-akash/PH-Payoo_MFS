b55e41c6f6870b038ed8e4bc89a74a85d7e63dd6
kamruj-akash <source.akash@gmail.com>
2025-08-24 13:02:40 +0600

    added js file

--- js/homePage.js
+++ js/homePage.js
@@ -1,32 +1,126 @@
+// set value
+// import { accountPinNo } from "./index";
+const couponCode = "GET500";
+
+// get value as a number
+function getNumberValue(id) {
+  return parseInt(document.getElementById(id).value);
+}
+
+// get value as default
+function getDefaultValue(id) {
+  return document.getElementById(id).value;
+}
+
+// innerText get value
+function getInnerValue(id) {
+  return parseInt(document.getElementById(id).innerText);
+}
+
+// addMoney
 document
   .getElementById("add_money_btn")
   .addEventListener("click", function (e) {
-    e.preventDefault();
-    const addMoneyBankName = document.getElementById("addMoney_bank");
-    const addMoneyAccountNo = document.getElementById(
-      "addMoney_account_no"
-    ).value;
-    const addMoneyAmount = parseInt(
-      document.getElementById("addMoney_amount").value
-    );
-    const accountPinNo = document.getElementById("account_pin").value;
-    const availableBalance = parseInt(
-      document.getElementById("available_balance").innerText
-    );
-    if (addMoneyAccountNo.length === 11) {
-      if (accountPinNo.length === 4) {
-        if (addMoneyAmount > 0) {
-          let admoneyAvailableBalance = availableBalance + addMoneyAmount;
+    e.defaultPrevented;
+    const addMoneyAccountNo = getDefaultValue("addMoney_account_no");
+    const availableBalance = getInnerValue("available_balance");
+    const addAmount = getNumberValue("addMoney_amount");
+    const addMoneyPin = getNumberValue("addMoney_pin");
+    const addMoneyBalance = availableBalance + addAmount;
+
+    if (addMoneyAccountNo.length === 1) {
+      if (accountPinNo === addMoneyPin) {
+        if (addAmount > 0) {
           document.getElementById("available_balance").innerText =
-            admoneyAvailableBalance;
+            addMoneyBalance;
         } else {
-          alert("Please provide correct amount");
+          alert("Enter Valid Amount");
         }
       } else {
-        alert("Please provide 4 digit PIN");
+        alert("Enter Valid PIN No");
       }
     } else {
-      alert("Please provide 11 digit Account No");
+      alert("Enter Valid Account No");
+    }
+  });
+
+// Cash Out
+document.getElementById("withdraw_btn").addEventListener("click", function (e) {
+  e.defaultPrevented;
+  const availableBalance = getInnerValue("available_balance");
+  const cashOutAccountNo = getDefaultValue("withdraw_account_no");
+  const cashOutPin = getNumberValue("withdraw_pin");
+  const cashOutAmount = getNumberValue("withdraw_amount");
+  const cashOutBalance = availableBalance - cashOutAmount;
+
+  if (cashOutAccountNo.length === 1) {
+    if (accountPinNo === cashOutPin) {
+      if (cashOutAmount > 0) {
+        document.getElementById("available_balance").innerText = cashOutBalance;
+      } else {
+        alert("Enter Valid Amount");
+      }
+    } else {
+      alert("Enter Valid PIN No");
+    }
+  } else {
+    alert("Enter Valid Account No");
+  }
+});
+
+// Transfer Money
+document
+  .getElementById("transferAmount_btn")
+  .addEventListener("click", function (e) {
+    e.defaultPrevented;
+    const availableBalance = getInnerValue("available_balance");
+    const transferAmount = getNumberValue("transferMoney_amount");
+    const transferAccountNo = getDefaultValue("transferMoney_account_no");
+    const transferPin = getNumberValue("transfer_pin");
+    const transferBalance = availableBalance - transferAmount;
+
+    if (transferAccountNo.length === 1) {
+      if (accountPinNo === transferPin) {
+        if (transferAmount > 0) {
+          document.getElementById("available_balance").innerText =
+            transferBalance;
+        } else {
+          alert("Enter Valid Amount");
+        }
+      } else {
+        alert("Enter Valid PIN No");
+      }
+    } else {
+      alert("Enter Valid Account No");
+    }
+  });
+
+// Get Bonus
+document
+  .getElementById("getBonus_amount_btn")
+  .addEventListener("click", function (e) {
+    e.defaultPrevented;
+    const availableBalance = getInnerValue("available_balance");
+    const inputCouponCode = getDefaultValue("getBonus_Coupon");
+    if (couponCode === inputCouponCode) {
+      const getBonusBalance = availableBalance - 500;
+      document.getElementById("available_balance").innerText = getBonusBalance;
+    } else {
+      alert("Enter a Valid Coupon Code");
+    }
+  });
+
+// Transfer Money
+document
+  .getElementById("PayBilling_btn")
+  .addEventListener("click", function (e) {
+    e.defaultPrevented;
+    const availableBalance = getInnerValue("available_balance");
+    const PayBillAmount = getNumberValue("payBill_amount");
+    const PayBillPin = getNumberValue("payBill_pin");
+    const PayBillBalance = availableBalance - PayBillAmount;
+    if (accountPinNo === PayBillPin) {
+      document.getElementById("available_balance").innerText = PayBillBalance;
     }
   });
 
--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 const login = document.getElementById("login_btn");
-const loginMobile = "1";
-const loginPin = "1";
+const loginMobile = "12345678910";
+const accountPinNo = "1234";
+export { accountPinNo };
 login.addEventListener("click", function (e) {
   e.preventDefault();
   const inputMobile = document.getElementById("input_mobileNo").value;
   const inputPin = document.getElementById("input_PinNo").value;
-  if (inputMobile === loginMobile && inputPin === loginPin) {
+  if (inputMobile === loginMobile && inputPin === accountPinNo) {
     window.location.href = "../homePage.html";
   } else {
     alert("Incorrect Mobile or Password");
